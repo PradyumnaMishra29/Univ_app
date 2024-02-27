a4c5533dee1942262172a02a1cb862c033542c4f
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-27 18:07:40 +0530

    add sticky footer

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,27 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const { environment } = require('@rails/webpacker');
+
+const webpack = require('webpack');
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+}));
+
+// Add Materialize to the JavaScript load path
+environment.loaders.append('sass', {
+  test: /\.scss$/,
+  use: [
+    {
+      loader: 'sass-loader',
+      options: {
+        additionalData: `@import "materialize";`
+      }
+    }
+  ]
+});
+
+module.exports = environment;
+
