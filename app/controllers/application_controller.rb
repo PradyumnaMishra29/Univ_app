7e1416b8e8a3026a39d9f43ac9f53deb7179d496
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-29 16:41:01 +0530

    add login page for student

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+	helper_method :current_user, :logged_in?
+	def current_user
+		@current_user ||= Student.find(session[:student_id]) if session[:student_id]
+		# ||= return the result unless it change(don't need to check our database everytime)
+	end
+
+	def logged_in?
+		!!current_user # !! - used to show boolean value
+	end
+
 end
