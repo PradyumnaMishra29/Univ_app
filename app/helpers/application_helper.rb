0b6d4e0625470b9a8817e3efdbae5083475849a7
Pradyumna Mishra <pradyumna.mishra@definelabs.com>
2024-02-29 19:02:04 +0530

    Course enroll from ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
 
-	if (logged_in?)
-      link_to ("logout", logout_path,method: :delete)
-    else
-      link_to ("login", login_path)
-    end
+	def session_link
+		if (logged_in?)
+	      link_to("logout", logout_path,method: :delete)
+	    else
+	      link_to("login", login_path)
+	    end
+	end
 
 end
